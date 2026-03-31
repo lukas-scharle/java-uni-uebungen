b0375016b2852f219b53f0deb7d5df69befcca4a
Lukas <lukas.scharle@gmail.com>
2026-03-31 06:59:18 +0200

    S2_Uebungsblatt_1 Aufgabe 1

---
+++ 2S_Uebungsblatt_1/src/Blatt1/Methoden.java
@@ -0,0 +1,10 @@
+package Blatt1;
+
+public class Methoden {
+    public static void druckeFiguren() {
+        System.out.println("**********    *          *******");
+        System.out.println("*             ***        *****");
+        System.out.println("*             *****      ***");
+        System.out.println("**********    *******    *");
+    }
+}
---
+++ 2S_Uebungsblatt_1/src/Main.java
@@ -0,0 +1,15 @@
+//TIP Zum <b>Ausführen</b> des Codes <shortcut actionId="Run"/> drücken oder
+// Klicke auf das Symbol <icon src="AllIcons.Actions.Execute"/> in der Randleiste.
+import Blatt1.Methoden;
+
+void main() {
+    // Aufgabe 1
+    for (int i = 0; i < 10; i++) {
+        Methoden.druckeFiguren();
+        System.out.println();
+    }
+}
+
+
+
+
