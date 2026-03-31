423dc5601fd22d9a31c65a10d17c4040c0bafa97
lukas-scharle <lukas.scharle@gmail.com>
2026-03-31 18:06:14 +0200

    S2_Uebungsblatt_1 Aufgabe 2 fertig

--- 2S_Uebungsblatt_1/src/Main.java
+++ 2S_Uebungsblatt_1/src/Main.java
@@ -1,13 +1,21 @@
 //TIP Zum <b>Ausführen</b> des Codes <shortcut actionId="Run"/> drücken oder
 // Klicke auf das Symbol <icon src="AllIcons.Actions.Execute"/> in der Randleiste.
-import Blatt1.Methoden;
 
+import Blatt1.Methoden;
 void main() {
     // Aufgabe 1
+    System.out.println("Aufgabe 1");
     for (int i = 0; i < 10; i++) {
         Methoden.druckeFiguren();
         System.out.println();
     }
+    System.out.println("----------");
+
+    // Aufgabe 2
+    System.out.println("Aufgabe 2");
+    Methoden.berechneAufgabe2(Methoden.readInt("Erste Zahl eingeben: "), Methoden.readInt("Zweite Zahl eingeben: "));
+    System.out.println("----------");
+
 }
 
 
